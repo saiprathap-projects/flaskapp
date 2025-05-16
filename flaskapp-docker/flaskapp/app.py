47bd115535c8f2a23cfce4012136b0c5e2dd7560
Saiprathap <tosaiprathapb@gmail.com>
2025-05-16 22:25:07 +0530

    Update app.py

--- flaskapp-docker/flaskapp/app.py
+++ flaskapp-docker/flaskapp/app.py
@@ -1,54 +1,25 @@
-'''from flask import Flask
+# app.py
+from flask import Flask, jsonify, request
+import requests
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    city = request.args.get('city')
+    if not city:
+        return jsonify({'error': 'Missing city parameter'}), 400
 
-if __name__ == "__main__":
-    app.run()
-'''
-from flask import Flask, render_template, request 
-  
-# import json to load JSON data to a python dictionary 
-import json 
-  
-# urllib.request to make a request to api 
-import urllib.request 
-  
-  
-app = Flask(__name__) 
-  
-@app.route('/', methods =['POST', 'GET']) 
-def weather(): 
-    if request.method == 'POST': 
-        city = request.form['city'] 
-    else: 
-        # for default name mathura 
-        city = 'mathura'
-  
-    # your API key will come here 
-    api = '3cd98018f5dc8e61267f3931b1cbff89'
-  
-    # source contain json data from api 
-    source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q =' + city + '&appid =' + api).read() 
-  
-    # converting JSON data to a dictionary 
-    list_of_data = json.loads(source) 
-  
-    # data for variable list_of_data 
-    data = { 
-        "country_code": str(list_of_data['sys']['country']), 
-        "coordinate": str(list_of_data['coord']['lon']) + ' ' 
-                    + str(list_of_data['coord']['lat']), 
-        "temp": str(list_of_data['main']['temp']) + 'k', 
-        "pressure": str(list_of_data['main']['pressure']), 
-        "humidity": str(list_of_data['main']['humidity']), 
-    } 
-    print(data) 
-    return render_template('index.html', data = data) 
-  
-  
-  
-if __name__ == '__main__': 
-    app.run(debug = True)
+    api_key = '3cd98018f5dc8e61267f3931b1cbff89'  # Replace with your API key
+    openweathermap_api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    response = requests.get(openweathermap_api_url)
+    data = response.json()
+
+    return jsonify({
+        'temperature': data['main']['temp'],
+        'humidity': data['main']['humidity'],
+        'wind_speed': data['wind']['speed']
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
