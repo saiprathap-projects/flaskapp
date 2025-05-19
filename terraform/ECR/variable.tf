87cffda50af5231c4edf2ab4ed806b18bbd8b86b
root <root@ip-172-31-17-12.ec2.internal>
2025-05-19 20:45:23 +0000

    update

---
+++ terraform/ECR/variable.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+}
+variable "repository_name" {
+  description = "The AWS region to deploy resources"
+  type        = string
+}
