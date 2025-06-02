bb90f00dc877c72c19fd9215fbe7ed4d75d07fb7
Saiprathap <tosaiprathapb@gmail.com>
2025-06-02 11:38:08 +0530

    Update variable.tf

--- terraform/ECR/variable.tf
+++ terraform/ECR/variable.tf
@@ -6,3 +6,7 @@ variable "repository_name" {
   description = "The AWS region to deploy resources"
   type        = string
 }
+variable "repository_name_1" {
+  description = "The AWS region to deploy resources"
+  type        = string
+}
